1f76099c35f6912ea9b92a5e9437294edccfa3c6
rajue <rajue@Raju>
2025-05-09 08:45:36 +0530

    initial commit

---
+++ VRIABLESDEMO/src/com/tnsif/variablesdemo/DemoTypesVar.java
@@ -0,0 +1,21 @@
+package com.tnsif.variablesdemo;
+
+public class DemoTypesVar {
+	//instance variable
+		int numone;
+		
+		//method
+		void display() {
+			//local variable
+			int numtwo=20;
+			System.out.println("this is a local variable" + numtwo);
+		}
+		
+		//static variable
+		static int numthree=30;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
